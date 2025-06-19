0ee41db534d11bb4b86d15ad5c37b75a9f13f2d8
syooy3 <eong213@snu.ac.kr>
2025-06-19 18:13:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,45 +12,16 @@ const foods = [
   'pizza.png'
 ];
 
-const reels = [
-  document.querySelector('.reel1'),
-  document.querySelector('.reel2'),
-  document.querySelector('.reel3')
-];
+function spinReel(reelClass, delay) {
+  const reel = document.querySelector(reelClass);
+  let i = 0;
+  const interval = setInterval(() => {
+    reel.style.backgroundImage = `url(${foods[i % foods.length]})`;
+    i++;
+  }, 100);
 
-const spinButton = document.getElementById('spin-button');
-
-// 버튼 인터랙션
-spinButton.addEventListener('mouseover', () => {
-  spinButton.src = 'button_hover.png';
-});
-spinButton.addEventListener('mouseout', () => {
-  spinButton.src = 'button_default.png';
-});
-spinButton.addEventListener('mousedown', () => {
-  spinButton.src = 'button_pressed.png';
-});
-spinButton.addEventListener('mouseup', () => {
-  spinButton.src = 'button_default.png';
-});
-
-spinButton.addEventListener('click', () => {
-  // 릴에 애니메이션 적용
-  let intervals = [];
-
-  for (let i = 0; i < 3; i++) {
-    let reel = reels[i];
-    let count = 0;
-    
-    intervals[i] = setInterval(() => {
-      const randomIndex = Math.floor(Math.random() * foods.length);
-      reel.style.backgroundImage = `url(${foods[randomIndex]})`;
-      count++;
-    }, 100);
-  }
-
-  // 1초 간격으로 릴 멈춤
-  setTimeout(() => clearInterval(intervals[0]), 1000);
-  setTimeout(() => clearInterval(intervals[1]), 2000);
-  setTimeout(() => clearInterval(intervals[2]), 3000);
-});
+  setTimeout(() => {
+    clearInterval(interval);
+    reel.style.backgroundImage = `url(${foods[Math.floor(Math.random() * foods.length)]})`;
+  }, delay);
+}
